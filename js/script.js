5feb13096eb249102f839b6e3735f443ef2e9c1d
P T <ptzencode@gmail.com>
2019-03-10 16:44:45 -0500

    pagination added

--- js/script.js
+++ js/script.js
@@ -18,4 +18,52 @@ const showPage = (list,page) => {
     }
 };
 
-showPage(completeList,1);
+const appendPageLinks = (list) => {
+    // eg. for 54 students, 6 total pages
+    let totalPages = Math.ceil(list.length / 10);
+    console.log(totalPages);
+
+    let div = document.createElement('div');
+    div.className = 'pagination';
+    let ul = document.createElement('ul');
+
+    div.appendChild(ul);
+    pageDiv.appendChild(div);
+
+    for (let i = 1; i <= totalPages; i++) {
+        let li = document.createElement('li');
+        let pageLink = document.createElement('a');
+        pageLink.setAttribute('href', '#');
+        pageLink.textContent = i;
+        li.appendChild(pageLink);
+        ul.appendChild(li);
+    }
+
+    ul.addEventListener('click', (e) => {
+        //e.preventDefault();
+        if(e.target.tagName === 'A') {
+            let clickedLink = e.target;
+            let allLinks = document.querySelectorAll('.pagination a');
+            for (let i = 0; i < allLinks.length; i++) {
+                if (allLinks[i] === clickedLink) {
+                    clickedLink.classList.add('active');
+                    let page = parseInt(clickedLink.textContent);
+                    //show list of students for current page
+                    showPage(list, page);
+                } else{
+                    allLinks[i].classList.remove('active');
+                }
+            }
+
+        }
+    });
+
+    // initially display the first page
+    let firstPage = document.querySelector('.pagination a');
+    firstPage.classList.add('active');
+    showPage(list,1);
+};
+
+window.onload = function() {
+    appendPageLinks(completeList);
+};
